2541ea0151f4fecf60c1048b6edb66a9012538cd
Kenniusboggs <ogbogukenny@yahoo.com>
2023-07-15 16:34:00 +0100

    Still refactoring...

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState, } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectTotalQTY, setOpenCart } from '../app/CartSlice.js';
 
@@ -30,10 +30,12 @@ const Navbar = () => {
             window.removeEventListener('scroll', onNavScroll);
         }
     },[]);
+
 return (
    <>
-      <header className={
-        !navState ? 'absolute top-7 left-0 right-0 opacity-100 z-50' : 'fixed top-0 left-0 right-0 h-[9vh] flex items-center justify-center opacity-100 z-[200] blur-effect-theme'
+      <header className={!navState 
+            ? 'absolute top-7 left-0 right-0 opacity-100 z-50' 
+            : 'fixed top-0 left-0 right-0 h-[9vh] flex items-center justify-center opacity-100 z-[200] blur-effect-theme'
       }>
         <nav className='flex items-center justify-between nike-container'>
             <div className='flex items-center'>
@@ -44,18 +46,34 @@ return (
                 />
             </div>
             <ul className='flex items-center justify-center gap-2'>
+
                 <li className='grid items-center'>
-                    <MagnifyingGlassIcon className={`icon-style ${navState && "text-slate-900 transition-all duration-300"}`} />
+                    <MagnifyingGlassIcon className={`icon-style 
+                        ${navState && "text-slate-900 transition-all duration-300"}`} />
                 </li>
+
                 <li className='grid items-center'>
-                    <HeartIcon className={`icon-style ${navState && "text-slate-900 transition-all duration-300"}`} />
+                    <HeartIcon className={`icon-style 
+                        ${navState && "text-slate-900 transition-all duration-300"}`} />
                 </li>
+
                 <li className='grid items-center'>
-                    <button type='button' onClick={onCartToggle} className='border-none outline-none active:scale-110 transition-all duration-300 relative'>
-                        <ShoppingBagIcon className={`icon-style ${navState && "text-slate-900 transition-all duration-300"}`} />
-                        <div className={`absolute top-4 right-0 shadow w-4 h-4 text-[0.65rem] leading-tight font-medium rounded-full flex items-center justify-center cursor-pointer hover:scale-110 transition-all duration-300 ${navState ? 'bg-slate-900 text-slate-100 shadow-slate-900' : 'bg-slate-100 text-slate-900 shadow-slate-100'}`}>{totalQTY}</div>
+                    <button type='button' onClick={onCartToggle} 
+                        className='border-none outline-none active:scale-110 transition-all duration-300 relative'>
+
+                        <ShoppingBagIcon 
+                            className={`icon-style 
+                                ${navState && "text-slate-900 transition-all duration-300"}`} />
+
+                        <div className={`absolute top-4 right-0 shadow w-4 h-4 text-[0.65rem] leading-tight 
+                            font-medium rounded-full flex items-center justify-center cursor-pointer 
+                            hover:scale-110 transition-all duration-300 
+                            ${navState 
+                            ? 'bg-slate-900 text-slate-100 shadow-slate-900' 
+                            : 'bg-slate-100 text-slate-900 shadow-slate-100'}`}>{totalQTY}</div>
                     </button>
                 </li>
+
             </ul>
         </nav>
       </header>
